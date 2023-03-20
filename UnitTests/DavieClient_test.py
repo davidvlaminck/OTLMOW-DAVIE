1649daf5d12a8f7c2d9f0460efffd014c95537b1
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-03-20 22:23:49 +0100

    added unittest for crt

--- UnitTests/DavieClient_test.py
+++ UnitTests/DavieClient_test.py
@@ -6,9 +6,17 @@ from otlmow_davie.Enums import AuthenticationType, Environment
 THIS_DIR = Path(__file__).parent
 
 
-def test_init_client():
+def test_init_client(subtests):
     settings_path = Path(THIS_DIR / 'settings_unittests.json')
-    davie_client = DavieClient(settings_path=settings_path, auth_type=AuthenticationType.JWT,
-                               environment=Environment.tei)
-    assert davie_client is not None
-    assert davie_client.rest_client is not None
+    with subtests.test(msg='JWT tei'):
+        davie_client = DavieClient(settings_path=settings_path, auth_type=AuthenticationType.JWT,
+                                   environment=Environment.tei)
+        assert davie_client is not None
+        assert davie_client.rest_client is not None
+
+    with subtests.test(msg='cert prd'):
+        davie_client = DavieClient(settings_path=settings_path, auth_type=AuthenticationType.cert,
+                                   environment=Environment.prd)
+        assert davie_client is not None
+        assert davie_client.rest_client is not None
+
